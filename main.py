9746157b1ff3764238c6d8ec3b2440dd083df919
Vladimir Veselinov <vladimirveselinov12@gmail.com>
2025-02-14 16:03:06 +0100

    add2

--- main.py
+++ main.py
@@ -12,25 +12,30 @@ def index():
         selected_image = request.form.get('image-selector')
 
         # Asignación #2. Recepción del texto
-        
+        text_top = request.form.get('textTop')
+        text_bottom = request.form.get('textBottom')
 
         # Assignment #3. Receiving the text's positioning
-       
+        selected_color =request.form.get('color-selector')
 
         # Asignación #3. Recepción del posicionamiento del texto
-        
+        text_top_y =request.form.get('textTop_y')
+        text_bottom_y =request.form.get('textBottom_y')
 
         return render_template('index.html', 
                                # Visualización de la imagen seleccionada
                                selected_image=selected_image, 
 
                                # Asignación #2. Visualización del texto
-                               
+                               text_top = text_top,
+                               text_bottom = text_bottom,
 
                                #  Asignación #3. Visualización del color
-                               
+                               selected_color = selected_color,
                                
                                # Asignación #3. Visualización de la posición del texto
+                               text_top_y = text_top_y,
+                               text_bottom_y = text_bottom_y
 
 
                                )
